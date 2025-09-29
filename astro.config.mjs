32e781566e71dc7e81b18a056a42d531ff0d9f8b
Lissa Aguilar <lissa.aguilar@gmail.com>
2025-09-29 12:42:16 -0700

    Update astro.config.mjs with github url site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'l4st4ct.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
