efe88c53e1d4352ad65f8b9eef3e22af7d8fe1e9
l4st4ct <lissa.aguilar@gmail.com>
2025-10-15 16:53:37 -0400

    updated works ts format

--- src/config/work.ts
+++ src/config/work.ts
@@ -1,117 +1,21 @@
 uxCaseStudy = {
     [
     {
-uxName: "XO Group ",
-pName:
-challenge:
-result:
-role:
-client:
-problem:
-details:"Validating Wedding Tools Usage
-Persona Development, User Research, Storyboards,  Wireframes, Prototypes
-
-
-Fil in missing areas
-
-
-
-I often worked on various projects at the XO Group during the discovery or evaluation phases of a product's lifecycle. Below are the various product experiences and reflections.
-
-
-
-
-
-
-<image>page8.png
-Caption
-Alt text
-Description
-
-
-
-Client
-Development, Design teams
-
-Problem
-Development and design teams were not practicing user-centered design and wanted help incorporating methods into their process starting with using personas.
-Role
-UX designer
-Challenge
-
-    Proto-personas were created the previous year when the teams adopted Agile, but were eventually abandoned.
-
-    The teams were using a generic user - Annie - in their user stories. Annie completed all tasks and did not reflect any of the existing user behaviors.
-
-Project Details
-I worked with the marketing and SEO teams to gather quantitative data on our users and their behaviors. I also worked closely with our UX researcher to gather qualitative data. Casey Skinnell and Amy O'Brien were selected as our primary personas representing either end of our user spectrum. I collaborated well with the design team to create trading cards and posters of our new persona brides. A launch party with games and scenarios were used to introduce the development teams to our personas. A month later developer user stories were found in Pivotal Tracker using at least one of the personas.
-
-Reflection
-Although we monitored story tickets  and saw an increase in using these personas in feature stories, I would have created more defined usage metrics that matched the goals and team needs.
-
-<image>page8b.png
-Caption
-Alt text
-Description
-
-
-
-Client
-The Marketplace team
-
-Role
-Information Architect, UX Designer
-
-Problem
-The  Marketplace was interested in incorporating the Budgeter tool into the  Wedding Planner App. 
-
-Challenge
-
-    Wedding Planner originally existed only on the website
-
-    adjustments to the Planner App as well as the Budgeter tool were required.
-
-Project Details
-There was no quantitative data  available for the existing Budgeter tool. I decided to start with  researching competitors and their solutions. I used Amy as our primary  persona, to come up with different sketches to solve her challenges in  organizing her budget for her wedding.
-
-After sharing the  high-fidelity wireframes and workflows with the product manager, I  proceeded to create Marvel App prototypes for user testing.  I  collaborated with our UX researcher to design tests to validate our  assumptions about how brides would use the Budgeter tool. 
-
-Tools Used
-Omnigraffle, Marvel App
-
-
-<image>/page13.png
-Caption
-Alt text
-Description
-
-
-Client
-The Knot Shop team
-
-Role
-Information Architect, UX Design
-
-Problem
-Current product pages on The Knot Shop online store were experiencing high cart abandonment rates. Client hypothesized the layout - particularly the  "Add to Cart" location - was the issue.
-
-Challenge
-
-    The client asked for alternative layouts to solve the problem.
-
-    The website code had layout limitations
-
-Project Details
-I  met with the product manager and tech lead for the Knot Shop to learn more about the problem. Unfortunately there was no tracking on existing pages, thus no information on what part of the pages were successful. I  then met with our UX researcher to help design tests to learn more about existing pages and elements on the page (circled in red). The findings helped me create different layouts that were later used in A/B tests.
-
-Tools Used
-Omnigraffle, Optimizely
-
-Request password to view full UX portfolio, lissa.aguilar@gmail.com
-<button> link to portfolio page
-Review full ux portfolio",
-
-tool:
+    uxName: "XO Group ",
+    pName: "",
+    challenge: "",
+    result: "",
+    role: "",
+    client: "",
+    problem:"",
+    details:"Validating Wedding Tools Usage
+    tags: "Persona Development, User Research, Storyboards,  Wireframes, Prototypes",   
+    alt_text: "",
+    Description: "",
+    Reflection: " ",
+    Tools Used: "mnigraffle, Marvel App",
+    Project Details: " ",
+    Tools:" Omnigraffle, Optimizely",
 
 }];
 }
