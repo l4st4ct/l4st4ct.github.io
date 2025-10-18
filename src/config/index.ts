18b412ed3db26f036011d8b44f05cd260e482dc6
l4st4ct <lissa.aguilar@gmail.com>
2025-10-18 15:41:25 -0400

    added case studies folder and md file for xogroup

--- src/config/index.ts
+++ src/config/index.ts
@@ -91,7 +91,7 @@ export const SITE_CONTENT: SiteContent = {
       name: "XO Group | The Knot",
       summary: "10-12 word count",
       role: 'tbd',
-      linkPreview: "/",
+      linkPreview: "/ux case studies/xogroup.md",
       linkSource: "https://github.com/immois/astro-zen",
       image: "/clone-ig.png",
     },
