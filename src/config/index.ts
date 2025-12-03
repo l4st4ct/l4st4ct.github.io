198b8c637fac2cbf39f22a91236c4664260e1e9e
l4st4ct <lissa.aguilar@gmail.com>
2025-12-03 12:55:33 -0500

    updated spacing in geniussports.md, pearson.md and index.ts file

--- src/config/index.ts
+++ src/config/index.ts
@@ -73,7 +73,7 @@ export const SITE_CONTENT: SiteContent = {
       role: 'Senior UX Designer',
       linkPreview: "/studies/pearson",
       linkSource: "https://github.com/immois/astro-zen",
-      image: "/[Registry] Charity FAQ_charity_wireframe_snpshot.png",
+      image: "/[Registry] Charity FAQ_charity_wireframe_snpshot.pngxxxxxx",
     },
     {
       name: "Genius Sports Group",
